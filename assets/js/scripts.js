4d900a2f9bc97b81a9fa0f59b2f3b6152c5c3a06
Michael Anokye <michaelanokyej@yahoo.com>
2019-09-11 14:35:35 -0400

    Working on setting icons

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,7 +3,8 @@ function userLocation(){
     let lat;
     let tempDescription = $('.temp-description');
     let tempDegree = $('.temp-degree');
-    // let tempDegree = $('.temp-degree');
+    let locationTimezone = $('.location-timezone');
+    // let weatherIcon = $('.icon')    
 
 
 
@@ -14,27 +15,35 @@ function userLocation(){
             long = position.coords.longitude;
             lat = position.coords.latitude;
             console.log(`long = ${long} and lat = ${lat} `);
+
+            const proxy = 'https://cors-anywhere.herokuapp.com/';
+            const weatherURL =`${proxy}https://api.darksky.net/forecast/0c2f73d8efc4b088e536ea18dc777461/${lat},${long}`;
+            fetch(weatherURL)
+            .then(response => response.json()) 
+            .then(data => {
+                const {temperature, summary, icon} = data.currently;
+                // Display elements in DOM
+                tempDegree.text(temperature);
+                tempDescription.text(summary);
+                locationTimezone.text(data.timezone);
+                // Set weather icon in DOM
+                // setIcons(icon, $('.icon'));
+                
+                })
         }); 
     }else{
         alert ('Your browser does not support geolocation');
     }
-    getWeather(long, lat)
-    // getuvIndex(long, lat);
-}
 
-function getWeather(long, lat){
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const weatherURL =`${proxy}https://api.darksky.net/forecast/0c2f73d8efc4b088e536ea18dc777461/${lat},${long}`;
-
-    fetch(weatherURL)
-        .then(response => response.json()) 
-        .then(data => {
-            console.log(data);
-            const {temperature, summary} = data.currently;
-        })
-
+// function setIcons(icon, iconID){
+//     const skycons = new Skycons({color: "white" });
+//     const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+//     skycons.play();
+//     return skycons.set(iconID, Skycons[currentIcon]);
+// }
 
 }
+
 
 
 
