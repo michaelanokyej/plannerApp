1b12bc94a0d8d05403654971ac48d418211bd111
Michael Anokye <michaelanokyej@yahoo.com>
2019-09-11 21:38:55 -0400

    using a new reverse geocoding API

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,11 @@
+'use strict'
+
 function userLocation(){
     let long;
     let lat;
     let tempDescription = $('.temp-description');
     let tempDegree = $('.temp-degree');
+    let tempSuggestion = $('.temp-suggestion');
     let locationTimezone = $('.location-timezone');
     // let weatherIcon = $('.icon')    
 
@@ -28,8 +31,10 @@ function userLocation(){
                 locationTimezone.text(data.timezone);
                 // Set weather icon in DOM
                 // setIcons(icon, $('.icon'));
-                
+                // let displayedTemp = temperature;
+                // getSuggestion(displayedTemp);
                 })
+            getUserCityAndState(long, lat)
         }); 
     }else{
         alert ('Your browser does not support geolocation');
@@ -43,7 +48,29 @@ function userLocation(){
 // }
 
 }
+// function to display suggestion 
+// function getSuggestion(displayedTemp){
+//     if(temperature < 75){
+//         tempSuggestion.text(`It is kinda cold, wear a sweater.`)
+//     }
+// }
 
+
+    function getUserCityAndState(long, lat){
+       const addURL = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${long}&key=AIzaSyCKIaru7a0pwY59xFUcRlsJz70biT9C730`;
+       fetch(addURL)
+    .then(response => {
+        if(response.ok){
+            return response.json();
+        }
+        throw new Error(response.statusText)
+    })
+    .then(responseJson => {console.log(responseJson);})
+    .catch(err => {
+      $('.errorMessage').text(`Something went wrong: ${err.message}`);
+    });
+    // getParks()
+    }
 
 
 
